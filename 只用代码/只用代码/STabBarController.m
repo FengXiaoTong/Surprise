6320a6940269fe845324922cbfe1301c7158a083
zeqi <wokuaibiyelea@163.com>
2016-01-18 17:34:27 +0800

    安装github上第三方软件KSImageNamed-Xcode-master，可以在Xcode里面自己提示图片！但是在tabbarController里面图片和字体都是蓝色的，与需求不符，需要改动

--- 只用代码/只用代码/STabBarController.m
+++ 只用代码/只用代码/STabBarController.m
@@ -32,21 +32,30 @@
     //1.home 首页
     UIViewController *home = [[UIViewController alloc]init];
     home.view.backgroundColor = [UIColor redColor];
+    home.tabBarItem.title = @"首页";
+    home.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
     [self addChildViewController:home];
     
     //2.message 信息
     UIViewController *message = [[UIViewController alloc]init];
     message.view.backgroundColor = [UIColor yellowColor];
+    message.tabBarItem.title = @"信息";
+    message.tabBarItem.image = [UIImage imageNamed:@"tabbar_message_center"];
     [self addChildViewController:message];
     
     //3.discover 发现
     UIViewController *discover = [[UIViewController alloc]init];
     discover.view.backgroundColor = [UIColor grayColor];
+    discover.tabBarItem.title = @"发现";
+    discover.tabBarItem.image = [UIImage imageNamed:@"tabbar_discover"];
     [self addChildViewController:discover];
+    
     
     //4.profile 我
     UIViewController *profile = [[UIViewController alloc]init];
     profile.view.backgroundColor = [UIColor purpleColor];
+    profile.tabBarItem.title = @"我";
+    profile.tabBarItem.image = [UIImage imageNamed:@"tabbar_profile"];
     [self addChildViewController:profile];
 }
 

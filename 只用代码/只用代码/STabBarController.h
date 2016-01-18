54ee79c96ab16ee8ff74111f533f6781ea51be42
zeqi <wokuaibiyelea@163.com>
2016-01-18 21:06:42 +0800

    各种封装就可以简化代码，更改流畅，可观。如果通过模型设置空间的文字颜色，只能通过文本属性（富文本：颜色，字体，空芯，文字阴影，图文混排）模型找不到属性，就找其父类的属性

--- 只用代码/只用代码/STabBarController.h
+++ 只用代码/只用代码/STabBarController.h
@@ -10,4 +10,6 @@
 
 @interface STabBarController : UITabBarController
 
+
+
 @end
--- 只用代码/只用代码/STabBarController.m
+++ 只用代码/只用代码/STabBarController.m
@@ -21,47 +21,70 @@
     [self setUpAllChildViewControllers];
 }
 
+#pragma mark -- 方法二选一
+//加载类的时候调用 ，什么时候调用：程序一启动，就会把所有的类加载进内存
+//+(void)load{
+//    
+//}
+
+
+//什么时候调用：当第一次使用这个类或者子类的时候调用   作用：初始化类
++(void)initialize{
+    
+    //获取所有的tabBarItem
+    UITabBarItem *item = [UITabBarItem appearance];
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark ---添加所以的视图控制器
 //封装思想：项目中有相同的功能，抽取一个类，封装好，自己做自己的事情。
+//继承于UIView的是控件，控件有颜色属性，继承于NSObject的是模型，模型用来保存数据,模型没有颜色属性！
+//如果通过模型设置控件的文字颜色，只能通过文本属性（富文本：颜色，字体，空芯，文字阴影，图文混排）
 -(void)setUpAllChildViewControllers
 {
     //管理添加tabVC的子视图
     //1.home 首页
     UIViewController *home = [[UIViewController alloc]init];
+    [self setUpOneChildViewControll:home image:[UIImage imageNamed:@"tabbar_home"] selectedImage:[UIImage imageWithOriginalName:@"tabbar_home_selected"] title:@"首页"];
     home.view.backgroundColor = [UIColor redColor];
-    home.tabBarItem.title = @"首页";
-    home.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
-    home.tabBarItem.selectedImage = [UIImage imageWithOriginalName:@"tabbar_home_selected"];
-    [self addChildViewController:home];
-    
+   
     //2.message 信息
     UIViewController *message = [[UIViewController alloc]init];
+    [self setUpOneChildViewControll:message image:[UIImage imageNamed:@"tabbar_message_center"] selectedImage:[UIImage imageWithOriginalName:@"tabbar_message_center_selected"] title:@"信息"];
     message.view.backgroundColor = [UIColor yellowColor];
-    message.tabBarItem.title = @"信息";
-    message.tabBarItem.image = [UIImage imageNamed:@"tabbar_message_center"];
-    message.tabBarItem.selectedImage = [UIImage imageWithOriginalName:@"tabbar_message_center_selected"];
-    [self addChildViewController:message];
     
     //3.discover 发现
     UIViewController *discover = [[UIViewController alloc]init];
+    [self setUpOneChildViewControll:discover image:[UIImage imageNamed:@"tabbar_discover"] selectedImage:[UIImage imageWithOriginalName:@"tabbar_discover_selected"] title:@"发现"];
     discover.view.backgroundColor = [UIColor grayColor];
-    discover.tabBarItem.title = @"发现";
-    discover.tabBarItem.image = [UIImage imageNamed:@"tabbar_discover"];
-    discover.tabBarItem.selectedImage = [UIImage imageWithOriginalName:@"tabbar_discover_selected"];
-    [self addChildViewController:discover];
-    
-    
+   
     //4.profile 我
     UIViewController *profile = [[UIViewController alloc]init];
+    [self setUpOneChildViewControll:profile image:[UIImage imageNamed:@"tabbar_profile"] selectedImage:[UIImage imageWithOriginalName:@"tabbar_profile_selected"] title:@"我"];
     profile.view.backgroundColor = [UIColor purpleColor];
-    profile.tabBarItem.title = @"我";
-    profile.tabBarItem.image = [UIImage imageNamed:@"tabbar_profile"];
-    profile.tabBarItem.selectedImage = [UIImage imageWithOriginalName:@"tabbar_profile_selected"];
-    [self addChildViewController:profile];
+
+}
+
+#pragma mark -- 添加一个子视图控制器
+-(void)setUpOneChildViewControll:(UIViewController *)vc image:(UIImage *)image selectedImage:(UIImage *)selectedImage title:(NSString *)title
+{
+    vc.tabBarItem.title = title;
+    vc.tabBarItem.image = image;
+    vc.tabBarItem.selectedImage = selectedImage;
+    
+    NSMutableDictionary *atts = [NSMutableDictionary dictionary];
+    atts[NSForegroundColorAttributeName] = [UIColor orangeColor];
+    //[atts setObject:[UIColor orangeColor] forKey:NSForegroundColorAttributeName];等价于上面的
+    [vc.tabBarItem setTitleTextAttributes:atts forState:UIControlStateSelected];
+    vc.tabBarItem.badgeValue = @"15";
+    [self addChildViewController:vc];
+
 }
 
 @end

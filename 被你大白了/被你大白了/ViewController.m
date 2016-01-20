223fcbba6617d8c727bf15ff72bb6e1745852cd8
zeqi <wokuaibiyelea@163.com>
2016-01-20 20:18:10 +0800

    集成语音合成成功： 我写，它帮我读出来,语音听写功能，不知道写到哪里了#83

--- 被你大白了/被你大白了/ViewController.m
+++ 被你大白了/被你大白了/ViewController.m
@@ -35,29 +35,29 @@
      //所有服务启动前，需要确保执行createUtility
     [IFlySpeechUtility createUtility:initString];
     
-//    //创建合成对象，为单例模式
-//    _iFlySpeechSynthesizer = [IFlySpeechSynthesizer sharedInstance];
-//    _iFlySpeechSynthesizer.delegate = self;
-//    
-//    //设置语音合成的参数
-//    //合成的语速,取值范围 0~100
-//    [_iFlySpeechSynthesizer setParameter:@"50" forKey:[IFlySpeechConstant SPEED]];
-//    //合成的音量;取值范围 0~100
-//    [_iFlySpeechSynthesizer setParameter:@"50" forKey:[IFlySpeechConstant VOLUME]];
-//    //发音人,默认为”xiaoyan”;可以设置的参数列表可参考个性化发音人列表
-//    [_iFlySpeechSynthesizer setParameter:@"小燕" forKey:[IFlySpeechConstant VOICE_NAME]];
-//    //音频采样率,目前支持的采样率有 16000 和 8000
-//    [_iFlySpeechSynthesizer setParameter:@"8000" forKey:[IFlySpeechConstant SAMPLE_RATE]];
-//    ////asr_audio_path保存录音文件路径，如不再需要，设置value为nil表示取消，默认目录是documents
-//    [_iFlySpeechSynthesizer setParameter:@"tts.pcm" forKey:[IFlySpeechConstant TTS_AUDIO_PATH]];
+    //创建合成对象，为单例模式
+    _iFlySpeechSynthesizer = [IFlySpeechSynthesizer sharedInstance];
+    _iFlySpeechSynthesizer.delegate = self;
+    
+    //设置语音合成的参数
+    //合成的语速,取值范围 0~100
+    [_iFlySpeechSynthesizer setParameter:@"50" forKey:[IFlySpeechConstant SPEED]];
+    //合成的音量;取值范围 0~100
+    [_iFlySpeechSynthesizer setParameter:@"50" forKey:[IFlySpeechConstant VOLUME]];
+    //发音人,默认为”xiaoyan”;可以设置的参数列表可参考个性化发音人列表
+    [_iFlySpeechSynthesizer setParameter:@"小燕" forKey:[IFlySpeechConstant VOICE_NAME]];
+    //音频采样率,目前支持的采样率有 16000 和 8000
+    [_iFlySpeechSynthesizer setParameter:@"8000" forKey:[IFlySpeechConstant SAMPLE_RATE]];
+    ////asr_audio_path保存录音文件路径，如不再需要，设置value为nil表示取消，默认目录是documents
+    [_iFlySpeechSynthesizer setParameter:@"tts.pcm" forKey:[IFlySpeechConstant TTS_AUDIO_PATH]];
     
     //启动合成会话
-    //[_iFlySpeechSynthesizer startSpeaking:@"您好，我是科大讯飞的小雅"];
+    [_iFlySpeechSynthesizer startSpeaking:@"您好，我是科大讯飞的小雅"];
     
-    ////隐藏键盘，点击空白处
-//    UITapGestureRecognizer *tapGr = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
-//    tapGr.cancelsTouchesInView = NO;
-//    [self.view addGestureRecognizer:tapGr];
+    //隐藏键盘，点击空白处
+    UITapGestureRecognizer *tapGr = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewTapped:)];
+    tapGr.cancelsTouchesInView = NO;
+    [self.view addGestureRecognizer:tapGr];
     
     //重新集成
     _iflyRecognizerView = [[IFlyRecognizerView alloc]initWithCenter:self.view.center];
@@ -74,63 +74,64 @@
 }
 
 
-//-(void)viewTapped:(UITapGestureRecognizer*)tapGr
-//{
-//    [self.textField resignFirstResponder];
-//}
+//取消键盘：textField 失去第一响应
+-(void)viewTapped:(UITapGestureRecognizer*)tapGr
+{
+    [self.textField resignFirstResponder];
+}
 
 
 
 - (IBAction)Click:(UIButton *)sender {
     
     //启动合成会话
-    //[_iFlySpeechSynthesizer startSpeaking:self.textField.text];
+    [_iFlySpeechSynthesizer startSpeaking:self.textField.text];
     
 }
 
 
 
-//#pragma mark - IFlySpeechSynthesizerDelegate
+#pragma mark - IFlySpeechSynthesizerDelegate
 
 //合成结束，必须要实现此代理
-//-(void)onCompleted:(IFlySpeechError *)error
-//{
-//    
-//}
+-(void)onCompleted:(IFlySpeechError *)error
+{
+    
+}
 
 
-////开始播放
-//- (void) onSpeakBegin
-//{
-//    
-//}
-//
-////缓冲进度
-//- (void) onBufferProgress:(int) progress message:(NSString *)msg
-//{
-//    NSLog(@"bufferProgress:%d,message:%@",progress,msg);
-//}
-//
-////播放进度
-//- (void) onSpeakProgress:(int) progress
-//{
-//    NSLog(@"play progress:%d",progress);
-//}
-//
-////暂停播放
-//- (void) onSpeakPaused
-//{
-//    
-//}
-//
-////恢复播放
-//- (void) onSpeakResumed
-//{
-//    
-//}
-//
+//开始播放
+- (void) onSpeakBegin
+{
+    
+}
+
+//缓冲进度
+- (void) onBufferProgress:(int) progress message:(NSString *)msg
+{
+    NSLog(@"bufferProgress:%d,message:%@",progress,msg);
+}
+
+//播放进度
+- (void) onSpeakProgress:(int) progress
+{
+    NSLog(@"play progress:%d",progress);
+}
+
+//暂停播放
+- (void) onSpeakPaused
+{
+    
+}
+
+//恢复播放
+- (void) onSpeakResumed
+{
+    
+}
+
 ////结束回调
-//- (void) onCompleted:(IFlySpeechError *) error
+//-(void) onCompleted:(IFlySpeechError *) error
 //{
 //    
 //}
@@ -149,9 +150,6 @@
 {
     
 }
-
-
-
 
 
 
